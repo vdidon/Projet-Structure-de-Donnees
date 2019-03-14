a44edaf71e92d2aa4f1f63846231c10fe4204c1a
vdidon <valmax$1>
2019-03-14 17:04:23 +0100

    Ajout des fonctions de suppresion

--- main.cpp
+++ main.cpp
@@ -3,14 +3,81 @@
 
 using namespace std;
 
-agenda a{};
+agenda ag{};
+
+void majuscule(char &c) {
+    if (c >= 'a' && c <= 'z')
+        c -= 'a' - 'A';
+}
+
+bool sortir() {
+    do {
+        cout << "Voulez vous revenir au menu ? (O/N) ";
+        char c;
+        cin >> c;
+        majuscule(c);
+        switch (c) {
+            case 'O':
+                return true;
+            case 'N':
+                return false;
+            default:
+                cout << "Entré O ou N" << endl;
+        }
+    } while ("poules" != "dents");
+}
+
+void entrerPersonne(string &nom, string &prenom) {
+    cout << "Entré le nom : ";
+    cin >> nom;
+    cout << "Entré le prenom : ";
+    cin >> prenom;
+}
+
+void entreJour(int &j, int &m, int &a) {
+    cout << "Entré le jour : ";
+    cin >> j;
+    cout << "Entré le mois : ";
+    cin >> m;
+    cout << "Entré l'année : ";
+    cin >> a;
+}
+
+void entreRdv(string &nom, int &j, int &m, int &a) {
+    cout << "Entré le nom : ";
+    cin >> nom;
+    entreJour(j, m, a);
+}
 
 void suppPersonne() {
-
+    string nom, prenom;
+    bool ext = false;
+    do {
+        entrerPersonne(nom, prenom);
+        if (ag.supprimerContact(nom, prenom))
+            ext = true;
+        else {
+            cout << "La personne ne peut pas être supprimé" << endl;
+            if (sortir())
+                ext = true;
+        }
+    } while (!ext);
 }
 
 void suppRdv() {
-
+    string nom;
+    int j, m, a;
+    bool ext = false;
+    do {
+        entreRdv(nom, j, m, a);
+        if (ag.supprimerRdv(j, m, a, nom))
+            ext = true;
+        else {
+            cout << "Le rdv ne peut pas être supprimé" << endl;
+            if (sortir())
+                ext = true;
+        }
+    } while (!ext);
 }
 
 void modifPersonne() {
@@ -46,7 +113,7 @@ void quitter() {
 }
 
 bool menuPrincipal() {
-    cout << "Menu principal : " << endl << endl;
+    cout << endl << "Menu principal : " << endl << endl;
     cout << "1 - Suppression d’une personne" << endl;
     cout << "2 - Suppression d’un rendez-vous" << endl;
     cout << "3 - Modification d’une personne" << endl;
