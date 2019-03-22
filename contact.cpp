c63e38f82674ec47961e10f355e26dcdce782c74
bahboubacar96 <48293451+bahboubacar96@users.noreply.github.com>
2019-03-22 09:21:56 +0100

    Update contact.cpp

--- contact.cpp
+++ contact.cpp
@@ -1,11 +1,10 @@
 #include "contact.h"
 #include "rdv.h"
 #include "temps.h" // pour ajouter un rdv à un contact (test si il est dispo)
-
+#include "ostream"
 contact::contact(const string &nom, const string &prenom, const string &num, const string &adresse) :
-        d_nom{nom}, d_prenom{prenom}, d_num{num}, d_adresseMail{adresse} {
-
-}
+        d_nom{nom}, d_prenom{prenom}, d_num{num}, d_adresseMail{adresse}
+{}
 
 string contact::getNom() const {
     return d_nom;
@@ -27,8 +26,7 @@ vectorLite<rdv *> &contact::getTabrdv() {
     return d_tabRdv;
 }
 
-void
-contact::modifNum(const string &num)// pourquoi la m�thode retourne un bool alors que c'est le contact qui est retourn�
+void contact::modifNum(const string &num)// pourquoi la m�thode retourne un bool alors que c'est le contact qui est retourn�
 {
     d_num = num;
 }
@@ -37,7 +35,45 @@ void contact::modifEmail(const string &adresse) {
     d_adresseMail = adresse;
 }
 
-bool contact::ajouterRdv(rdv *r) {
-    d_tabRdv.push_back(r);
+bool contact::operator==(const contact &c) const
+{
+    return (d_nom == c->d_nom && d_prenom == c->d_prenom);
 }
 
+bool contact::ajouterRdv(rdv *r)
+{
+    for(int i=0; i<d_tabRdv.size(); i++)
+    {
+        if(!d_tabRdv[i]->pasEnMemeTemps(r))
+        {
+            return false;
+        }
+    }
+    d_tabRdv.push_back(r);
+    return true;
+}
+
+bool contact::supprimerRdv(rdv *r)
+{
+    if (r)
+    {
+        int i=0;
+        while (i<d_tabRdv.size() && d_tabRdv[i] != r)
+        {
+            i++;
+        }
+        supprimerRdv(i);
+        return true;
+    }
+    return false;
+}
+
+
+void contact ::afficherRdvDeContact(std::ostream & os)
+{
+}
+
+contct ::~contact()
+{
+
+}
