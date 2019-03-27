8b13965afd7d9cfba640941a44434215562d1353
Valère DIDON <vdidon@live.fr>
2019-03-27 11:06:32 +0100

    plus opti

--- temps.cpp
+++ temps.cpp
@@ -8,43 +8,36 @@ temps::temps(short t_h, short t_m) :
 
 unsigned int temps::duree() const{
     unsigned int temp;
-    temp = h * 60 + m;
+	temp = static_cast<unsigned int>(h * 60 + m);
     return temp;
 }
 
 
 unsigned int temps::dureeComparee(const temps &t) const{
     unsigned int temp;
-    temp = (t.h * 60 + t.m) - (h * 60 + m);     //Unsigned => retourne la valeur absolue si l'heure de this
+	temp = static_cast<unsigned int>((t.h * 60 + t.m) -
+	                                 (h * 60 + m));     //Unsigned => retourne la valeur absolue si l'heure de this
     return temp;                                 // est plus grande que l'heure de t ?
 }
 
 bool temps::operator>(const temps &t) const {
-    if (duree() > t.duree())
-        return true;
+	return duree() > t.duree();
 
-    return false;
 }
 
 bool temps::operator<(const temps &t) const {
-    if (duree() < t.duree())
-        return true;
+	return duree() < t.duree();
 
-    return false;
 }
 
 bool temps::operator==(const temps &t) const {
-    if (duree() == t.duree())
-        return true;
+	return duree() == t.duree();
 
-    return false;
 }
 
 bool temps::operator!=(const temps &t) const {
-    if (duree() != t.duree())
-        return true;
+	return duree() != t.duree();
 
-    return false;
 }
 
 bool temps::operator<=(const temps &t) const {
